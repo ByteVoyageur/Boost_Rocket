b232d49ed638c30546fb71655910c12a5ee8f809
Xiaosong <bixiaosong@gmail.com>
2024-11-28 17:23:54 +0100

    Rocket can be fly with 1000

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 100f;
     Rigidbody rb;
 
     private void Start()
@@ -21,8 +22,7 @@ public class Movement : MonoBehaviour
     {
         if(thrust.IsPressed())
         {
-            rb.AddRelativeForce(0, 10, 0);
-            Debug.Log("Dang, I need some space");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 }
