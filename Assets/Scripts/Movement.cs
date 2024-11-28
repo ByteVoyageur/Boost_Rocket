66fe7b9c322b0d9a95f6b4858e91ba3438ff528f
Xiaosong <bixiaosong@gmail.com>
2024-11-28 18:24:42 +0100

    rocket rote well.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
     Rigidbody rb;
 
@@ -16,13 +17,33 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
         if(thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if(rotationInput < 0)
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(rotationInput > 0)
+        {
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
